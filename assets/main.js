4fec3c0e1372afddb521640c1b6e4e3c57ce7339
Umbeee <umberto.nesta@hotmail.com>
2023-04-13 13:54:58 +0200

    layout statico creato

--- assets/main.js
+++ assets/main.js
@@ -21,3 +21,14 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+
+const prev = document.querySelector('.indietro');
+const next = document.querySelector('.avanti');
+
+
+prev.addEventListener("click", function(){
+    indietro()
+})
+
+indietro()
